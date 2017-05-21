876e41704a7cfdfcda3bf7f1b711f1c201e00f2c
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-05-21 20:37:49 +0100

    Define to do list

---
+++ FMDatabasePopulator.py
@@ -0,0 +1,22 @@
+"""
+Database populator is designed primarily for populating the tables of a new database.
+The parameters specify the database language as well as the structure of that table. This file
+uses a series of other text files to populate the various fields in the tables.
+
+Author: Fionn Mcguire
+Date: 21/05/2017
+
+"""
+
+"""
+TODO LIST
+ - Take files as variables
+ - Allow users to specify seperators such as \n or ,
+ - Allow users to specify table structure as parameters
+ - Convert all parameters to lowercase
+ - Distinguish the parameters as variable types
+ - Check the data in the file match the datatype specified
+ - Create text file with series of insert statements
+ 
+"""
+
